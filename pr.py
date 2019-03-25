778a4832216b9cb7d2307aefbc666762845c34ef
jjjjjjjjjoyful <wldnjs0919@naver.com>
2019-03-25 20:25:04 +0900

    first commit

---
+++ pr.py
@@ -0,0 +1,8 @@
+lion_we_know = ['hyojin', 'seol', 'eunjin', 'kijung']
+lion_meet = ['hyojin', 'eunjin', 'bak']
+
+for lion in lion_meet:
+    if lion in lion_we_know:
+        print("hello!")
+    else:
+        print("i don't know")
